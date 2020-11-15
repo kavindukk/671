ea4a0cc555d381bce314355dbbaa163f7cd46b84
kavindu-ubuntu <kavindukusal@gmail.com>
2020-11-15 13:30:39 -0700

    batch square working

--- midterm2/Q2d.py
+++ midterm2/Q2d.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.integrate import odeint
+import matplotlib.pyplot as plt
 
 class recursive_least_square:
     def __init__(self):
@@ -8,16 +9,19 @@ class recursive_least_square:
         self.kalmanGain = np.array([[0,0,0]])
         self.QTranspose = np.array([[0,0,0]])
         self.Ts = 0.01
-        self.steps = 1000
+        self.steps = 100
         self.y0 = [0,0]
         self.t = [i*self.Ts for i in range(1,self.steps)]
         self.solution = odeint(self.system, self.y0, self.t)  
         self.YDoubleDot = np.zeros((self.steps))
         self.input = np.zeros((self.steps))
         self.Allcoeff = np.array([[0,0,0]])
+        self.batchQ = np.array([[0,0,0]])
+        self.batchH = np.array([0,0,0])
 
         self.calculate_values()  
         self.update_gains()
+        self.calculate_batch_least_squares()
 
     def system(self, x, t):
         f, f_dot = x
@@ -35,10 +39,12 @@ class recursive_least_square:
         self.input[step] = u
 
     def calculate_QTranspose(self,step):
-        i = -self.solution[step-1,1]
-        j = -self.solution[step-1,0]
-        k = self.input[step-1]
-        self.QTranspose =  np.array([[ i, j, k ]])
+        if step >=1:
+            i = -self.solution[step-1,1]
+            j = -self.solution[step-1,0]
+            k = self.input[step]
+            self.QTranspose =  np.array([[ i, j, k ]])
+            self.batchQ = np.append(self.batchQ, self.QTranspose, axis=0)
 
     def calculate_kalman_gain(self, step):        
         QT = self.QTranspose
@@ -60,6 +66,13 @@ class recursive_least_square:
         nextCoeff = h + self.kalmanGain * (realY - expectedY)
         self.Allcoeff = np.append(self.Allcoeff, h, axis=0)
         self.currCoeffT = nextCoeff
+    
+    def calculate_batch_least_squares(self):
+        A = self.batchQ
+        y = np.array([self.YDoubleDot]).T
+        h = np.linalg.inv( A.T @ A) @ A.T @ y
+        h= h.T 
+        self.batchH = h[0]
 
     def calculate_values(self):
         for i in range(self.steps):
@@ -81,6 +94,7 @@ rls = recursive_least_square()
 # print(rls.YDoubleDot)
 
 print(rls.Allcoeff)
+print(rls.batchH)
 # print(rls.input)
 # print(rls.P)
 
